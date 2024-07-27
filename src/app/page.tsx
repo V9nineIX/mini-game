9b18fa3b450f688b9406c495e5ac62450ad29ddf
Wi9 <v9nineix@gmail.com>
2024-07-27 14:57:34 +0700

    intigate game continer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,38 @@
+'use client';
+import React, { useEffect } from "react";
 import Image from "next/image";
 
+import { GameController } from "@/HTMLElementGameTemplete/GameController";
+import { GameElement } from "@/HTMLElementGameTemplete/GameElement";
+
+
+
+
 export default function Home() {
+
+
+  let frame = 0;
+  function start() {
+    new GameController("gameController", null, null);
+    setInterval(update, 1000 / 60);
+    console.log(window.innerWidth + " : " + window.innerHeight);
+  }
+
+  function update() {
+    frame++;
+    //SetStyle(assets["world"],(frame/128),(50+Math.sin(frame/128)*25),100 ,(frame*2),getRandomColor());
+    for (let id in GameElement.GameElementColletion) {
+      GameElement.GameElementColletion[id].update(
+        GameElement.GameElementColletion[id]
+      );
+    }
+  }
+
+  useEffect(() => {
+
+    console.log('mount')
+    start()
+  }, [])
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
